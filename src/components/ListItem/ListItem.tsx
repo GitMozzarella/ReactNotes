f12afcc9f2ca510e72b209cd415469bf4f56fe49
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-19 15:46:18 +0500

    feat: add ListItem markup

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,3 +1,41 @@
+import styles from './listitem.module.scss'
+import { MdDelete, MdModeEdit } from 'react-icons/md'
+
 export const ListItem = () => {
-	return <div></div>
+	return (
+		<>
+			<ul className={styles.list}>
+				<li className={styles.note}>
+					<p className={styles.headerNote}>Продукты</p>
+					<div className={styles.noteInfo}>
+						<p className={styles.timeNote}>20:17</p>
+						<p className={styles.textNote}>Additional text</p>
+					</div>
+					<div className={styles.buttons}>
+						<button className={styles.editButton}>
+							<MdModeEdit />
+						</button>
+						<button className={styles.deleteButton}>
+							<MdDelete />
+						</button>
+					</div>
+				</li>
+				<li className={styles.note}>
+					<p className={styles.headerNote}>Header</p>
+					<div className={styles.noteInfo}>
+						<p className={styles.timeNote}>05.08.2023</p>
+						<p className={styles.textNote}>Additional</p>
+					</div>
+					<div className={styles.buttons}>
+						<button className={styles.editButton}>
+							<MdModeEdit />
+						</button>
+						<button className={styles.deleteButton}>
+							<MdDelete />
+						</button>
+					</div>
+				</li>
+			</ul>
+		</>
+	)
 }
