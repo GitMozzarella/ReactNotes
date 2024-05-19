bd9ba2919cbe232c8ed04472e4f2cf01bf091e3d
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-19 15:48:33 +0500

    feat: добавил верхнюю панель

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,3 +1,10 @@
+import { AuthStatus } from '../../context/AuthStatus'
+import styles from './toolbar.module.scss'
+
 export const Toolbar = () => {
-	return <div></div>
+	return (
+		<div className={styles.toolbar}>
+			<AuthStatus />
+		</div>
+	)
 }
