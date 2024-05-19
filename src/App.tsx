89c242c5cf40c109cbb8484263ee277268905734
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-19 15:42:41 +0500

    feat: add Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
+import './App.scss'
+import { Router } from './router/Router'
 
 export const App = () => {
 	return (
 		<>
-			<div>app</div>
+			<Router />
 		</>
 	)
 }
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
+import { Home } from '../pages/Home'
+import { NotFound } from '../pages/NotFound'
+import { Content } from '../pages/Content'
+import { Login } from '../pages/Login'
+import { MainLayout } from '../components/MainLayout'
 
 export const Router = () => {
 	return (
 		<Routes>
-			<Route path='/'>Home</Route>
-			<Route></Route>
+			<Route element={<MainLayout />}>
+				<Route path='/' element={<Home />} />
+				<Route path='/notes/:id' element={<Content />} />
+			</Route>
+			<Route path='*' element={<NotFound />} />
+			<Route path='/auth' element={<Login />} />
 		</Routes>
 	)
 }
