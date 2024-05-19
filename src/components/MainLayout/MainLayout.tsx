d2e5ec4f55d73bc88dd4885e11d5ee55acfc7c89
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-19 15:46:49 +0500

    feat: add MainLayout with Outlet

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,3 +1,18 @@
+import { Sidebar } from '../Sidebar'
+import { Toolbar } from '../Toolbar'
+import styles from './layout.module.scss'
+import { Outlet } from 'react-router-dom'
+
 export const MainLayout = () => {
-	return <div></div>
+	return (
+		<div className={styles.layout}>
+			<Toolbar />
+			<div className={styles.content}>
+				<Sidebar />
+				<div className={styles.outlet}>
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	)
 }
