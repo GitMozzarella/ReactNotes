8fb05865b6c1c11138b799772a1325ab133c92ea
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-24 18:39:13 +0500

    chore: change router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,6 @@ import { Home } from '../pages/Home'
 import { NotFound } from '../pages/NotFound'
 import { Content } from '../pages/Content'
 import { SignIn } from '../pages/SignIn'
-import { SignUp } from '../pages/SignUp'
 import { MainLayout } from '../components/MainLayout'
 
 export const Router = () => {
@@ -15,7 +14,6 @@ export const Router = () => {
 			</Route>
 			<Route path='*' element={<NotFound />} />
 			<Route path='/auth' element={<SignIn />} />
-			<Route path='/registration' element={<SignUp />} />
 		</Routes>
 	)
 }
