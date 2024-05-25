92b1760bc70fa825ea7bfe367e0e986fb99df4cb
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-26 04:55:48 +0500

    feat: темная тема и стили для outleta

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,15 +1,18 @@
+import { useContext } from 'react'
+import { NotesContext } from '../../context/NotesProvider/NotesProvider'
 import { Sidebar } from '../Sidebar'
 import { Toolbar } from '../Toolbar'
 import styles from './layout.module.scss'
 import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
+	const { darkMode } = useContext(NotesContext)
 	return (
 		<div className={styles.layout}>
 			<Toolbar />
 			<div className={styles.content}>
 				<Sidebar />
-				<div className={styles.outlet}>
+				<div className={`${styles.outlet} ${darkMode ? styles.darkMode : ''}`}>
 					<Outlet />
 				</div>
 			</div>
