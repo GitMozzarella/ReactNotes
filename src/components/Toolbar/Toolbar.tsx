83cb00bdc937bf53f31981d354755c63d2a3ea4c
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-25 06:04:22 +0500

    feat: добавил контекст и переключение темы

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,10 +1,18 @@
+import { useContext } from 'react'
 import { AuthStatus } from '../../context/AuthStatus'
 import styles from './toolbar.module.scss'
+import { IoMdMoon, IoIosSunny } from 'react-icons/io'
+import { NotesContext } from '../../context/NotesProvider/NotesProvider'
 
 export const Toolbar = () => {
+	const { darkMode, toggleDarkMode } = useContext(NotesContext)
+
 	return (
-		<div className={styles.toolbar}>
+		<div className={`${styles.toolbar} ${darkMode ? styles.darkMode : ''}`}>
 			<AuthStatus />
+			<div className={styles.toggle} onClick={toggleDarkMode}>
+				{darkMode ? <IoIosSunny /> : <IoMdMoon />}
+			</div>
 		</div>
 	)
 }
