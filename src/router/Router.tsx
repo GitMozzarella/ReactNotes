1477353dee4de83832abd295e503fceff37a3bb0
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-30 03:19:32 +0500

    fix: добавил страницу на которой рендерится заметка для редактирования

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Home } from '../pages/Home'
 import { NotFound } from '../pages/NotFound'
 import { Content } from '../pages/Content'
 import { SignIn } from '../pages/SignIn'
+import { Note } from '../pages/Note'
 import { MainLayout } from '../components/MainLayout'
 
 export const Router = () => {
@@ -11,6 +12,7 @@ export const Router = () => {
 			<Route element={<MainLayout />}>
 				<Route path='/' element={<Home />} />
 				<Route path='/notes' element={<Content />} />
+				<Route path='/notes/:id' element={<Note />} />
 			</Route>
 			<Route path='*' element={<NotFound />} />
 			<Route path='/auth' element={<SignIn />} />
