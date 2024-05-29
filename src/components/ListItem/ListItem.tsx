4436dfd0febde4443ca517a423a399ff7350b049
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-30 00:48:46 +0500

    feat: добавил рендеринг массива заметок

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,41 +1,37 @@
+import { useContext } from 'react'
 import styles from './listitem.module.scss'
+import { NotesContext } from '../../context/NotesProvider/NotesProvider'
 import { MdDelete, MdModeEdit } from 'react-icons/md'
+import { getDate } from '../../utils/getDate'
 
 export const ListItem = () => {
+	const { notes } = useContext(NotesContext)
+
 	return (
-		<>
-			<ul className={styles.list}>
-				<li className={styles.note}>
-					<p className={styles.headerNote}>Продукты</p>
-					<div className={styles.noteInfo}>
-						<p className={styles.timeNote}>20:17</p>
-						<p className={styles.textNote}>Additional text</p>
-					</div>
-					<div className={styles.buttons}>
-						<button className={styles.editButton}>
-							<MdModeEdit />
-						</button>
-						<button className={styles.deleteButton}>
-							<MdDelete />
-						</button>
-					</div>
-				</li>
-				<li className={styles.note}>
-					<p className={styles.headerNote}>Header</p>
-					<div className={styles.noteInfo}>
-						<p className={styles.timeNote}>05.08.2023</p>
-						<p className={styles.textNote}>Additional</p>
-					</div>
-					<div className={styles.buttons}>
-						<button className={styles.editButton}>
-							<MdModeEdit />
-						</button>
-						<button className={styles.deleteButton}>
-							<MdDelete />
-						</button>
-					</div>
-				</li>
-			</ul>
-		</>
+		<ul className={styles.list}>
+			{notes.length === 0 ? (
+				<li className={styles.noNotes}>Нет заметок</li>
+			) : (
+				notes.map(note => (
+					<li key={note.id} className={styles.note}>
+						<p className={styles.headerNote}>{note.headerNote}</p>
+						<div className={styles.noteInfo}>
+							<p className={styles.timeNote}>
+								{note.date === getDate() ? note.time : note.date}
+							</p>
+							<p className={styles.textNote}>{note.textNote}</p>
+						</div>
+						<div className={styles.buttons}>
+							<button className={styles.editButton}>
+								<MdModeEdit />
+							</button>
+							<button className={styles.deleteButton}>
+								<MdDelete />
+							</button>
+						</div>
+					</li>
+				))
+			)}
+		</ul>
 	)
 }
