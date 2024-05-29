921c051a2e9c8e4dae888624899884f5359ef868
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-05-30 00:56:33 +0500

    feat: добавил в провайдер состояние notes и реализацию добавления новой заметки

--- src/context/NotesProvider/NotesProvider.tsx
+++ src/context/NotesProvider/NotesProvider.tsx
@@ -1,23 +1,50 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 import { useToggle } from '../../hooks/useToggle'
-
-export interface INote {}
+import { v4 as uuidv4 } from 'uuid'
+import { getCurrentTime } from '../../utils/getCurrentTime'
+import { getCurrentFullDate } from '../../utils/getCurrentFullDate'
+import { getDate } from '../../utils/getDate'
+export interface INote {
+	id: string
+	headerNote: string
+	time?: string
+	fullDate?: string
+	date?: string
+	textNote: string
+}
 
 export interface INotesContext {
 	darkMode: boolean
 	toggleDarkMode: () => void
+	notes: INote[]
+	addNote: () => void
 }
 
 export const NotesContext = createContext<INotesContext>({
 	darkMode: false,
-	toggleDarkMode: () => {}
+	toggleDarkMode: () => {},
+	notes: [],
+	addNote: () => {}
 })
 
 export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 	const [darkMode, toggleDarkMode] = useToggle(false)
+	const [notes, setNotes] = useState<INote[]>([])
+
+	const addNote = () => {
+		const newNote: INote = {
+			id: uuidv4(),
+			headerNote: 'Новая заметка',
+			time: getCurrentTime(),
+			fullDate: getCurrentFullDate(),
+			date: getDate(),
+			textNote: 'Текст заметки...'
+		}
+		setNotes([...notes, newNote])
+	}
 
 	return (
-		<NotesContext.Provider value={{ darkMode, toggleDarkMode }}>
+		<NotesContext.Provider value={{ darkMode, toggleDarkMode, notes, addNote }}>
 			{children}
 		</NotesContext.Provider>
 	)
