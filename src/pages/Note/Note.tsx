7e07315d52bf72b9aec3919f693202d0c1101699
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-01 13:13:02 +0500

    feat: добавил состояния редактирования заметки

--- src/pages/Note/Note.tsx
+++ src/pages/Note/Note.tsx
@@ -1,20 +1,33 @@
 import styles from './note.module.scss'
 import { useContext, useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { NotesContext, INote } from '../../context/NotesProvider/NotesProvider'
 
 export const Note = () => {
-	const { notes, updateNote } = useContext(NotesContext)
+	const {
+		notes,
+		updateNote,
+		setHeaderEdited,
+		setTextEdited,
+		headerEdited,
+		textEdited
+	} = useContext(NotesContext)
 	const { id } = useParams<{ id: string }>()
 	const [note, setNote] = useState<INote | undefined>(undefined)
+	const navigate = useNavigate()
 
 	useEffect(() => {
 		const foundNote = notes.find(note => note.id === id)
-		setNote(foundNote)
-	}, [id, notes])
+		if (!foundNote) {
+			navigate('/notfound')
+		} else {
+			setNote(foundNote)
+		}
+	}, [id, notes, navigate])
 
 	const handleHeaderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (note) {
+			setHeaderEdited(true)
 			const updatedNote = { ...note, headerNote: e.target.value }
 			setNote(updatedNote)
 			updateNote(updatedNote)
@@ -23,33 +36,35 @@ export const Note = () => {
 
 	const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 		if (note) {
+			setTextEdited(true)
 			const updatedNote = { ...note, textNote: e.target.value }
 			setNote(updatedNote)
 			updateNote(updatedNote)
 		}
 	}
 
-	if (!note) {
-		return <div>Заметка не найдена</div>
-	}
-
 	return (
 		<div className={styles.noteContainer}>
-			<p className={styles.fullDate}>{note.fullDate}</p>
-			<input
-				type='text'
-				value={note.headerNote}
-				placeholder='Новая заметка'
-				onChange={handleHeaderChange}
-				className={styles.headerNote}
-				maxLength={25}
-			/>
-			<textarea
-				value={note.textNote}
-				onChange={handleTextChange}
-				placeholder='Текст заметки...'
-				className={styles.textNote}
-			></textarea>
+			{note && (
+				<>
+					<p className={styles.fullDate}>{note.fullDate}</p>
+					<input
+						type='text'
+						value={headerEdited ? note.headerNote : ''}
+						placeholder='Новая заметка'
+						onChange={handleHeaderChange}
+						className={styles.headerNote}
+						maxLength={25}
+					/>
+					<textarea
+						value={textEdited ? note.textNote : ''}
+						placeholder='Текст заметки...'
+						onChange={handleTextChange}
+						className={styles.textNote}
+						maxLength={3500}
+					></textarea>
+				</>
+			)}
 		</div>
 	)
 }
