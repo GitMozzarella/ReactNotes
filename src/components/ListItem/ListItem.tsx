351a786a3a7e2efc75e455b3186f63afbb08d843
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-01 13:11:39 +0500

    feat: изначально сделал чтобы был placeholder

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -7,7 +7,6 @@ import { getDate } from '../../utils/getDate'
 
 export const ListItem = () => {
 	const { notes, deleteNote } = useContext(NotesContext)
-
 	const navigate = useNavigate()
 
 	const handleEditClick = (id: string) => {
@@ -21,12 +20,16 @@ export const ListItem = () => {
 			) : (
 				notes.map(note => (
 					<li key={note.id} className={styles.note}>
-						<p className={styles.headerNote}>{note.headerNote}</p>
+						<p className={styles.headerNote}>
+							{note.headerNote || 'Новая заметка'}
+						</p>
 						<div className={styles.noteInfo}>
 							<p className={styles.timeNote}>
 								{note.date === getDate() ? note.time : note.date}
 							</p>
-							<p className={styles.textNote}>{note.textNote}</p>
+							<p className={styles.textNote}>
+								{note.textNote || 'Текст заметки...'}
+							</p>
 						</div>
 						<div className={styles.buttons}>
 							<button
