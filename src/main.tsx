141aad519f029ec8b7b477ed45df3dfc2c3b9be8
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-02 15:19:48 +0500

    feat: add ModalsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { NotesProvider } from './context/NotesProvider/NotesProvider.tsx'
+import { ModalsProvider } from '@mantine/modals'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<MantineProvider>
 				<NotesProvider>
-					<App />
+					<ModalsProvider labels={{ confirm: 'Submit', cancel: 'Cancel' }}>
+						<App />
+					</ModalsProvider>
 				</NotesProvider>
 			</MantineProvider>
 		</BrowserRouter>
