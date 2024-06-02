e3e1bb1946f4a045b06f7e709e9b020e1e66a906
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-02 19:25:56 +0500

    feat: добавил кнопку очистки для инпута

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,15 +1,36 @@
-import { TextInput } from '@mantine/core'
+import React, { useState } from 'react'
+import { Input, CloseButton } from '@mantine/core'
 import { IoSearch } from 'react-icons/io5'
 import styles from './search.module.scss'
 
 export const SearchInput = () => {
+	const [value, setValue] = useState<string>('')
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newValue = event.target.value
+		setValue(newValue)
+	}
+
+	const handleClear = () => {
+		setValue('')
+	}
+
 	return (
-		<>
-			<TextInput
-				className={styles.searchInput}
-				leftSection={<IoSearch className={styles.icon} />}
-				placeholder='Поиск...'
-			></TextInput>
-		</>
+		<Input
+			type='text'
+			className={styles.searchInput}
+			leftSection={<IoSearch className={styles.icon} />}
+			value={value}
+			placeholder='Поиск...'
+			onChange={handleChange}
+			rightSectionPointerEvents='all'
+			rightSection={
+				<CloseButton
+					aria-label='Clear input'
+					onClick={handleClear}
+					style={{ display: value ? undefined : 'none' }}
+				/>
+			}
+		/>
 	)
 }
