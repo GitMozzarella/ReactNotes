ee1851906086bea9bd96deba001183e8a4977012
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-02 18:37:43 +0500

    feat: добавил scrollArea из mantine для списка заметок

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './listitem.module.scss'
-import { Button } from '@mantine/core'
+import { Button, ScrollArea } from '@mantine/core'
 import { NotesContext } from '../../context/NotesProvider/NotesProvider'
 import { MdDelete, MdModeEdit } from 'react-icons/md'
 import { getDate } from '../../utils/getDate'
@@ -15,42 +15,49 @@ export const ListItem = () => {
 	}
 
 	return (
-		<ul className={styles.list}>
-			{notes.length === 0 ? (
-				<li className={styles.noNotes}>Нет заметок</li>
-			) : (
-				notes.map(note => (
-					<li key={note.id} className={styles.note}>
-						<p className={styles.headerNote}>
-							{note.headerNote || 'Новая заметка'}
-						</p>
-						<div className={styles.noteInfo}>
-							<p className={styles.timeNote}>
-								{note.date === getDate() ? note.time : note.date}
+		<ScrollArea
+			type='scroll'
+			scrollbarSize={10}
+			offsetScrollbars
+			className={styles.scrollArea}
+		>
+			<ul className={styles.list}>
+				{notes.length === 0 ? (
+					<li className={styles.noNotes}>Нет заметок</li>
+				) : (
+					notes.map(note => (
+						<li key={note.id} className={styles.note}>
+							<p className={styles.headerNote}>
+								{note.headerNote || 'Новая заметка'}
 							</p>
-							<p className={styles.textNote}>
-								{note.textNote || 'Текст заметки...'}
-							</p>
-						</div>
-						<div className={styles.buttons}>
-							<Button
-								className={styles.editButton}
-								onClick={() => handleEditClick(note.id)}
-								variant='transparent'
-							>
-								<MdModeEdit />
-							</Button>
-							<Button
-								className={styles.deleteButton}
-								onClick={() => confirmDeleteNote(note.id)}
-								variant='transparent'
-							>
-								<MdDelete />
-							</Button>
-						</div>
-					</li>
-				))
-			)}
-		</ul>
+							<div className={styles.noteInfo}>
+								<p className={styles.timeNote}>
+									{note.date === getDate() ? note.time : note.date}
+								</p>
+								<p className={styles.textNote}>
+									{note.textNote || 'Текст заметки...'}
+								</p>
+							</div>
+							<div className={styles.buttons}>
+								<Button
+									className={styles.editButton}
+									onClick={() => handleEditClick(note.id)}
+									variant='transparent'
+								>
+									<MdModeEdit />
+								</Button>
+								<Button
+									className={styles.deleteButton}
+									onClick={() => confirmDeleteNote(note.id)}
+									variant='transparent'
+								>
+									<MdDelete />
+								</Button>
+							</div>
+						</li>
+					))
+				)}
+			</ul>
+		</ScrollArea>
 	)
 }
