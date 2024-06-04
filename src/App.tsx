db866974f9c10a44e99b2f44a44fec29a103aa0f
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-04 16:35:40 +0500

    feat: AuthProvider add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.scss'
+import { AuthProvider } from './context/AuthProvider'
 import { Router } from './router/Router'
 
 export const App = () => {
 	return (
-		<>
+		<AuthProvider>
 			<Router />
-		</>
+		</AuthProvider>
 	)
 }
