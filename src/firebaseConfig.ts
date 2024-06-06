0eb1b3ff91e799c39ca25a547855946ebe7a3545
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-06 10:36:13 +0500

    cfg: fix firebaseCfg

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,6 +5,8 @@ import { getAuth } from '@firebase/auth'
 const firebaseConfig = {
 	apiKey: 'AIzaSyCdNHWPce649k2WauuggfFvpdYS5bkf5o0',
 	authDomain: 'notes-eb92a.firebaseapp.com',
+	databaseURL:
+		'https://notes-eb92a-default-rtdb.europe-west1.firebasedatabase.app',
 	projectId: 'notes-eb92a',
 	storageBucket: 'notes-eb92a.appspot.com',
 	messagingSenderId: '2285902873',
