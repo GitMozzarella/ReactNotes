2e9ba58270aef4695e407704a3d9aa408fb6412f
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-14 19:19:23 +0500

    refactor: вынес интерфейсы в types

--- src/context/NotesProvider/NotesProvider.tsx
+++ src/context/NotesProvider/NotesProvider.tsx
@@ -20,30 +20,10 @@ import {
 	query,
 	where
 } from 'firebase/firestore'
-
-export interface INote {
-	id: string
-	headerNote: string
-	time?: string
-	fullDate?: string
-	date?: string
-	textNote: string
-	userId?: string
-}
-
-export interface INotesContext {
-	darkMode: boolean
-	toggleDarkMode: () => void
-	notes: INote[]
-	addNote: () => void
-	confirmDeleteNote: (id: string) => void
-	deleteNote: (id: string) => void
-	updateNote: (updatedNote: INote) => void
-	setHeaderEdited: (edited: boolean) => void
-	setTextEdited: (edited: boolean) => void
-	headerEdited: boolean
-	textEdited: boolean
-}
+import { INote, INotesContext } from '../../types/types'
+import { Path } from '../../router/Path'
+import { messages } from './../../constants/messages'
+import { ErrorMessages } from '../../constants/errorMessages'
 
 export const NotesContext = createContext<INotesContext>({
 	darkMode: false,
@@ -56,7 +36,8 @@ export const NotesContext = createContext<INotesContext>({
 	setHeaderEdited: () => {},
 	setTextEdited: () => {},
 	headerEdited: false,
-	textEdited: false
+	textEdited: false,
+	loading: true
 })
 
 export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
@@ -64,6 +45,7 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 	const [notes, setNotes] = useState<INote[]>([])
 	const [headerEdited, setHeaderEdited] = useState(false)
 	const [textEdited, setTextEdited] = useState(false)
+	const [loading, setLoading] = useState(true)
 	const [user, setUser] = useState(auth.currentUser)
 	const navigate = useNavigate()
 
@@ -80,6 +62,7 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 					notesArray.push({ id: doc.id, ...doc.data() } as INote)
 				})
 				setNotes(notesArray)
+				setLoading(false)
 			}
 		}
 		fetchNotes()
@@ -111,25 +94,33 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 	}
 
 	const deleteNote = async (id: string) => {
-		await deleteDoc(doc(db, 'notes', id))
-		setNotes(notes.filter(note => note.id !== id))
-		notifications.show({
-			title: 'Note deleted',
-			message: 'Your note was successfully deleted.',
-			autoClose: 3000,
-			color: 'blue'
-		})
-		navigate('/notes')
+		try {
+			await deleteDoc(doc(db, 'notes', id))
+			setNotes(notes.filter(note => note.id !== id))
+			notifications.show({
+				title: messages.notification,
+				message: messages.confirmDelete,
+				autoClose: 3000,
+				color: 'blue'
+			})
+			navigate(Path.notes)
+		} catch (error) {
+			console.error(ErrorMessages.errorDelete, error)
+			notifications.show({
+				title: ErrorMessages.error,
+				message: ErrorMessages.errorDeleteMessage,
+				autoClose: 3000,
+				color: 'red'
+			})
+		}
 	}
 
 	const confirmDeleteNote = (id: string) => {
 		modals.openConfirmModal({
-			title: 'Delete your note',
+			title: messages.delete,
 			centered: true,
-			children: (
-				<Text size='sm'>Are you sure you want to delete this note?</Text>
-			),
-			labels: { confirm: 'Delete', cancel: 'No' },
+			children: <Text size='sm'>{messages.confirmMessage}</Text>,
+			labels: { confirm: 'Да', cancel: 'Нет' },
 			confirmProps: { color: 'blue' },
 			onCancel: () => console.log('Cancel'),
 			onConfirm: () => deleteNote(id)
@@ -140,10 +131,16 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 		if (user) {
 			const currentTime = getCurrentTime()
 			const updatedNoteTime = { ...updatedNote, time: currentTime }
-			await updateDoc(doc(db, 'notes', updatedNote.id), updatedNoteTime)
-			setNotes(
-				notes.map(note => (note.id === updatedNote.id ? updatedNoteTime : note))
-			)
+			try {
+				await updateDoc(doc(db, 'notes', updatedNote.id), updatedNoteTime)
+				setNotes(
+					notes.map(note =>
+						note.id === updatedNote.id ? updatedNoteTime : note
+					)
+				)
+			} catch (error) {
+				console.error(ErrorMessages.errorUpdate, error)
+			}
 		}
 	}
 
@@ -160,7 +157,8 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 				setHeaderEdited,
 				setTextEdited,
 				headerEdited,
-				textEdited
+				textEdited,
+				loading
 			}}
 		>
 			{children}
