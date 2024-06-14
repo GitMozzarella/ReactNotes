13e314db785a8512a3e8f343d6855f53fc9b0cd0
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-14 20:00:19 +0500

    chore: add error messages for errorboundary

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -3,5 +3,10 @@ export enum ErrorMessages {
 	errorUpdate = 'Ошибка при обновлении заметки:',
 	errorDelete = 'Error deleting note:',
 	error = 'Error',
-	errorDeleteMessage = 'An error occurred while deleting the note.'
+	errorDeleteMessage = 'An error occurred while deleting the note.',
+	validateMessage = `Имя пользователя должно содержать только латинские буквы, одну заглавную .Не менее 4 символов. Запретные символы (!@#$%^&*_-=)`,
+	dataCatchError = 'Ошибка получения данных',
+	errorDidCatch = 'Сообщение об ошибке',
+	errorDidCatchInfo = 'Что-то пошло не так!',
+	ErrorAuth = 'Ошибка авторизации'
 }
