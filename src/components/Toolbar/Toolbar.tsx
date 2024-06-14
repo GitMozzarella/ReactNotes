954911e07b3a4cfb9b978b3060b9acdfd3476226
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-14 19:17:59 +0500

    feat: добавил home icon для переадресации на homepage

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,21 +1,29 @@
 import { useContext } from 'react'
 import { AuthStatus } from '../../context/AuthStatus'
 import styles from './toolbar.module.scss'
-import { IoMdMoon, IoIosSunny } from 'react-icons/io'
+import { IoMdMoon, IoIosSunny, IoIosHome } from 'react-icons/io'
 import { NotesContext } from '../../context/NotesProvider/NotesProvider'
+import { useNavigate } from 'react-router-dom'
+import { Path } from '../../router/Path'
 
 export const Toolbar = () => {
+	const navigate = useNavigate()
 	const { darkMode, toggleDarkMode } = useContext(NotesContext)
 
+	const redirectToHome = () => {
+		navigate(Path.home)
+	}
+
 	return (
-		<div
-			className={`${styles.toolbar} ${
-				darkMode ? styles.darkMode : styles.toolbar
-			}`}
-		>
+		<div className={`${styles.toolbar} ${darkMode ? styles.darkMode : ''}`}>
 			<AuthStatus />
-			<div className={styles.toggle} onClick={toggleDarkMode}>
-				{darkMode ? <IoIosSunny /> : <IoMdMoon />}
+			<div className={styles.icons}>
+				<div className={styles.toggle} onClick={redirectToHome}>
+					<IoIosHome />
+				</div>
+				<div className={styles.toggle} onClick={toggleDarkMode}>
+					{darkMode ? <IoIosSunny /> : <IoMdMoon />}
+				</div>
 			</div>
 		</div>
 	)
