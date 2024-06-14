655e45e60e78657c9409a28f544e610d25d64888
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-14 19:59:54 +0500

    feat: add RouteWithErrorBoundary

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -7,15 +7,37 @@ import { MainLayout } from '../components/MainLayout'
 import { PrivateRoute } from '../components/PrivateRoute'
 import { AuthenticationForm } from '../pages/AuthenticationForm/AuthenticationForm'
 import { Path } from './Path'
+import { RouteWithErrorBoundary } from '../components/RouteWithErrorBoundary'
 export const Router = () => {
 	return (
 		<Routes>
 			<Route path={Path.auth} element={<AuthenticationForm />} />
 			<Route element={<PrivateRoute />}>
 				<Route element={<MainLayout />}>
-					<Route path={Path.home} element={<Home />} />
-					<Route path={Path.notes} element={<Workspace />} />
-					<Route path={Path.note} element={<Note />} />
+					<Route
+						path={Path.home}
+						element={
+							<RouteWithErrorBoundary>
+								<Home />
+							</RouteWithErrorBoundary>
+						}
+					/>
+					<Route
+						path={Path.notes}
+						element={
+							<RouteWithErrorBoundary>
+								<Workspace />
+							</RouteWithErrorBoundary>
+						}
+					/>
+					<Route
+						path={Path.note}
+						element={
+							<RouteWithErrorBoundary>
+								<Note />
+							</RouteWithErrorBoundary>
+						}
+					/>
 				</Route>
 			</Route>
 			<Route path={Path.notfound} element={<NotFound />} />
